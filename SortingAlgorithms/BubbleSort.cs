47c2621b262d7696ef26e295a28873b1e996f58e
1SaiVaruna <sai030804@gmail.com>
2024-10-03 20:45:09 +0100

    Added 'Perform' method to sort list of doubles using bubble sort algorithm.

--- SortingAlgorithms/BubbleSort.cs
+++ SortingAlgorithms/BubbleSort.cs
@@ -8,5 +8,25 @@ namespace SortingAlgorithms
 {
     internal class BubbleSort
     {
+        public static void Perform(List<double> listToSort)
+        {
+            bool swapped = true;
+            double temp;
+
+            while (swapped)
+            {
+                swapped = false;
+                for (int i = 0; i < listToSort.Count - 1; i++)
+                {
+                    if (listToSort[i] > listToSort[i + 1])
+                    {
+                        temp = listToSort[i];
+                        listToSort[i] = listToSort[i + 1];
+                        listToSort[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+            }
+        }
     }
 }
