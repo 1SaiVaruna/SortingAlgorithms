12ba242197a8af28ce750f4381fe2ba8aa2c36bc
1SaiVaruna <sai030804@gmail.com>
2024-10-03 20:54:01 +0100

    Added method to populate and print list of random doubles.

--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -27,6 +27,35 @@
             BubbleSort.Perform(listOfDoubles);
             Console.WriteLine("Sort Completed");
         }
-    }
+
+        public static void TenDoubles()
+        {
+            Console.WriteLine("Populating list with random numbers...");
+            PopulateListWithRandomDoubles(10);
+            Console.WriteLine("List Populated");
+        }
+
+        static void PopulateListWithRandomDoubles(int size)
+        {
+            listOfDoubles.Clear();
+
+            for (int i = 0; i < size; i++)
+            {
+                double twoDigitDouble = Double.Parse(randomGenerator.NextDouble().ToString("0.00000"));
+                listOfDoubles.Add(twoDigitDouble);
+            }
+        }
+
+        public static void PrintListOfDoubles()
+        {
+            Console.WriteLine("\n\nLIST PRINT:\n");
+
+            for (int i = 0; i < listOfDoubles.Count; i++)
+            {
+                Console.WriteLine("  " + i + "  " + listOfDoubles[i].ToString());
+            }
+
+            Console.WriteLine("\nEND \n");
+        }
     }
 }
