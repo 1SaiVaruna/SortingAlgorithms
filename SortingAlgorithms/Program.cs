c4142f6040733c25f4d4c7da47ccf9bb846241cf
1SaiVaruna <sai030804@gmail.com>
2024-10-03 20:49:28 +0100

    Added 'randomGenerator' to generate random numbers. Added 'listOfDoubles' to store double values for sorting.

--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -2,6 +2,10 @@
 {
     internal class Program
     {
+
+        static Random randomGenerator = new Random();
+        static List<double> listOfDoubles = new List<double>();
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
