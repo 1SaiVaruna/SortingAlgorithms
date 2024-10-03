ce46314765b0c61dcaf02ca66cdc6d1b7167fd48
1SaiVaruna <sai030804@gmail.com>
2024-10-03 19:28:36 +0100

    Added 'MenuSystem' class with constructor.

--- SortingAlgorithms/MenuSystem.cs
+++ SortingAlgorithms/MenuSystem.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,17 @@ namespace SortingAlgorithms
 {
     internal class MenuSystem
     {
+        string callingClass;
+        Dictionary<string, MethodInfo> menuOptions;
+
+        public MenuSystem(string callingClassName)
+        {
+            callingClass = callingClassName;
+
+            menuOptions = new Dictionary<string, MethodInfo>();
+
+            Type thisType = typeof(Environment);
+            AddMenuItem("Exit Application", thisType.GetMethod("Exit"));
+        }
     }
 }
